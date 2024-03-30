dba0f56c30891837b0ad77d7d2f526cd48ff89ab
M B A R K <mbark1236@gmail.com>
2024-03-30 18:14:03 +0000

    Feat: First Initialization was finished

--- SQLDPresence/Form1.Designer.cs
+++ SQLDPresence/Form1.Designer.cs
@@ -30,27 +30,59 @@
         {
             this.components = new System.ComponentModel.Container();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // timer1
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Roboto", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(103)))), ((int)(((byte)(76)))), ((int)(((byte)(213)))));
+            this.label1.Location = new System.Drawing.Point(296, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SQLDPresence";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Roboto", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(237, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(328, 33);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "A Discord RPC by MBVRK";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(18)))), ((int)(((byte)(24)))));
+            this.ClientSize = new System.Drawing.Size(776, 420);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
+            this.ShowInTaskbar = false;
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- SQLDPresence/Form1.cs
+++ SQLDPresence/Form1.cs
@@ -1,36 +1,25 @@
 ﻿using DiscordRpcDemo;
-
-using EnvDTE;
-
-using EnvDTE80;
-
-
-
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Drawing;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Runtime.InteropServices.ComTypes;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-
 using Process = System.Diagnostics.Process;
+
 
 namespace SQLDPresence
 {
     public partial class Form1 : Form
     {
-        private DiscordRpc.EventHandlers handlers;
+        private DiscordRpc.EventHandlers handlers = default;
         private DiscordRpc.RichPresence presence;
+
+        private DateTime startTime      = DateTime.Now;
+        private bool     isSsmsFirstRun = true;
+
 
         public Form1()
         {
             InitializeComponent();
+            Microsoft.Win32.Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true).SetValue("MBVRK.SsmsRpc", Application.ExecutablePath);
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -39,108 +28,89 @@ namespace SQLDPresence
             CheckSsms();
         }
 
+
+        /// <summary>
+        /// Initializes the presence
+        /// </summary>
         private void InitializePresence()
         {
-            handlers = default(DiscordRpc.EventHandlers);
             DiscordRpc.Initialize("1223365681077293156", ref handlers, true, null);
-            handlers = default(DiscordRpc.EventHandlers);
-            DiscordRpc.Initialize("1223365681077293156", ref handlers, true, null);
-            //this.presence.details = "SQL Server";
-            presence.state = "Running";
-            presence.largeImageKey = "sql_server_logo";
-            //presence.smallImageKey = "8422322";
-            presence.largeImageText = "SQL Server";
-            //presence.smallImageText = "SQL File or Query";
-            //DiscordRpc.UpdatePresence(ref presence);
+            presence = new DiscordRpc.RichPresence();
         }
 
+
+        /// <summary>
+        /// Updates the presence
+        /// </summary>
+        private void UpdatePresence()
+        {
+            UpdatePresenceTime();
+            DiscordRpc.UpdatePresence(ref presence);
+        }
+
+
+        /// <summary>
+        /// Sets the presence image and text ( large image and details )
+        /// </summary>
+        private void SetPresenceImageAndText()
+        {
+            presence.largeImageText = "SQL Server";
+            presence.largeImageKey  = "sql_server_logo";
+
+            const string ssmsFullName = "SQL Server Management Studio";
+
+            presence.largeImageText = ssmsFullName;
+            presence.details        = ssmsFullName;
+        }
+
+
+        /// <summary>
+        /// Updates the presence time
+        /// </summary>
+        private void UpdatePresenceTime()
+        {
+            presence.state = (DateTime.Now - startTime).ToString(@"hh\:mm\:ss");
+        }
+
+        /// <summary>
+        /// Checks if SSMS ( SQL Server Management Studio ) is running
+        /// </summary>
         private void CheckSsms()
         {
-            bool isSsmsOpen = IsSsmsRunning();
+            var isSsmsOpen = IsSsmsRunning();
             if (isSsmsOpen)
             {
-                InitializePresence();
-
-                DTE2 dte = GetDte();
-                if (dte != null)
+                if ( isSsmsFirstRun )
                 {
-                    Window activeWindow = dte.ActiveWindow;
-                    if (activeWindow != null && activeWindow.Type == vsWindowType.vsWindowTypeDocument)
-                    {
-                        Document activeDocument = activeWindow.Document;
-                        if (activeDocument != null)
-                        {
-                            this.presence.smallImageKey = "8422322";
+                    InitializePresence();
+                    SetPresenceImageAndText();
 
-                            if (activeDocument.FullName.EndsWith(".sql", StringComparison.OrdinalIgnoreCase))
-                            {
-                                // User is inside a SQL file
-                                this.presence.details = "Editing SQL: " + activeDocument.Name;
-                            }
-                            else if (activeDocument.FullName.EndsWith(".query", StringComparison.OrdinalIgnoreCase))
-                            {
-                                // User is inside a query file
-                                this.presence.details = "Editing Query: " + activeDocument.Name;
-                            }
-                        }
-                    }
+                    startTime      = DateTime.Now;
+                    isSsmsFirstRun = false;
                 }
+
+                UpdatePresence();
             }
             else
             {
                 DiscordRpc.Shutdown();
-            }
-            DiscordRpc.UpdatePresence(ref this.presence);
-        }
-
-
-
-        bool IsSsmsRunning()
-        {
-            Process[] processes = Process.GetProcessesByName("Ssms");
-            return processes.Length > 0;
-        }
-
-        private DTE2 GetDte()
-        {
-            //DTE2 dte = null;
-            //Type typeDTE = Type.GetTypeFromProgID("VisualStudio.DTE.17.0", true);
-            //object objDTE = Activator.CreateInstance(typeDTE, true);
-            //dte = (DTE2)objDTE;
-            //return dte;
-
-
-            object runningObject = null;
-            IRunningObjectTable rot;
-            IEnumMoniker enumMoniker;
-            IMoniker[] moniker = new IMoniker[1];
-            IntPtr pfetched = IntPtr.Zero;
-
-            GetRunningObjectTable(0, out rot);
-            rot.EnumRunning(out enumMoniker);
-
-            while (enumMoniker.Next(1, moniker, pfetched) == 0)
-            {
-                IMoniker runningMoniker = moniker[0];
-                string name = null;
-
-                if (runningMoniker != null)
-                {
-                    runningMoniker.GetDisplayName(null, null, out name);
-
-                    if (name.Contains("VisualStudio.DTE"))
-                    {
-                        rot.GetObject(runningMoniker, out runningObject);
-                        break;
-                    }
-                }
+                isSsmsFirstRun = true;
             }
 
-            return runningObject as DTE2;
         }
 
-        [DllImport("ole32.dll")]
-        private static extern int GetRunningObjectTable(int reserved, out IRunningObjectTable pprot);
+
+        /// <summary>
+        /// Checks if SSMS ( SQL Server Management Studio ) is running
+        /// </summary>
+        /// <returns>
+        /// <c>true</c> if SSMS is running; otherwise, <c>false</c>.
+        /// </returns>
+        private static bool IsSsmsRunning()
+        {
+            return Process.GetProcesses().Any(process => process.ProcessName.Equals("Ssms", StringComparison.OrdinalIgnoreCase));
+        }
+
 
         private void timer1_Tick(object sender, EventArgs e)
         {
